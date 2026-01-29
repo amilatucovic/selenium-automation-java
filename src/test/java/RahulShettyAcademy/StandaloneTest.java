5e33255d1e310030aedbadd309301bdc2a85e33e
amilatucovic <tucovicamila@gmail.com>
2026-01-29 22:12:28 +0100

    Initial commit - Selenium with Java automation framework

---
+++ src/test/java/RahulShettyAcademy/StandaloneTest.java
@@ -0,0 +1,66 @@
+package RahulShettyAcademy;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+import java.util.List;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+
+public class StandaloneTest {
+
+    public static void main(String[] args) {
+
+        String productName = "ZARA COAT 3";
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        driver.get("https://rahulshettyacademy.com/client");
+
+        driver.findElement(By.id("userEmail")).sendKeys("amysantiago@brooklyn99.com");
+        driver.findElement(By.id("userPassword")).sendKeys("AmySantiago123#");
+        driver.findElement(By.id("login")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".mb-3")));
+        List<WebElement> products = driver.findElements(By.cssSelector(".mb-3"));
+
+        WebElement prod = products.stream()
+                .filter(product ->
+                        product.findElement(By.cssSelector("b"))
+                                .getText().equalsIgnoreCase(productName))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("ZARA COAT 3 not found"));
+        WebElement addToCartButton =
+                prod.findElement(By.cssSelector(".card-body button:last-of-type"));
+        wait.until(ExpectedConditions.elementToBeClickable(addToCartButton));
+        addToCartButton.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#toast-container")));
+        wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(".ng-animating"))));
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(".ngx-spinner-overlay")));
+        driver.findElement(By.cssSelector("[routerlink*='cart']")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".cartSection")));
+        List<WebElement> cartProducts = driver.findElements(By.cssSelector(".cartSection h3"));
+        Boolean match = cartProducts.stream().anyMatch(cartProduct -> cartProduct.getText().equalsIgnoreCase(productName));
+        Assert.assertTrue(match);
+        driver.findElement(By.cssSelector(".totalRow button")).click();
+
+
+        Actions a = new Actions(driver);
+        a.sendKeys(driver.findElement(By.cssSelector("[placeholder='Select Country']")), "india").build().perform();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ta-results")));
+        driver.findElement(By.xpath("(//button[contains(@class, 'ta-item')]) [2]")).click();
+        driver.findElement(By.cssSelector(".action__submit")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".hero-primary")));
+        String confirmationMessage = driver.findElement(By.cssSelector(".hero-primary")).getText();
+        Assert.assertTrue(confirmationMessage.equalsIgnoreCase("THANKYOU FOR THE ORDER."));
+        driver.close();
+
+
+
+    }
+
+}
